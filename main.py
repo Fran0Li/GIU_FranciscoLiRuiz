dfaa1e304a7a250cf58d05040c5bd4d738f8cf5a
Francisco Li <liruizfrancisco@gmail.com>
2026-03-25 19:49:18 -0600

    Importación de librerías y definición de variables globales y constantes para demás ventanas

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import pygame as pg
+
+#Variables globales
+win_open = None
+music_on = False
+
+#Constantes de diseño
+WIDTH = 500
+HEIGHT = 500
+ANI_W = 800
+ANI_H = 500
+RADIO = 25
+
