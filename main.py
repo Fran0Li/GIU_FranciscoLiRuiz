c98f16b91a48467aa60081990fb64608834dc502
Francisco Li <liruizfrancisco@gmail.com>
2026-03-25 22:19:05 -0600

    Finalización de ventana de análisis numérico y widgets para ventana principal

--- main.py
+++ main.py
@@ -46,6 +46,51 @@ def abrir_analisis_num ():
     window1.title("Análisis númerico")
     window1.geometry("500x500")
     window1.resizable(False, False)
-   
+    def cerrar_window1():
+        global win_open
+        win_open = None
+        window1.destroy()
+    canva2 = tk.Canvas(window1, bg="#F028D2", width=WIDTH, height=HEIGHT)
+    canva2.pack(fill="both", expand=True)
+    tk.Label(canva2, text="Ingrese un número entero:").pack(pady=10)
+    entrada_text = tk.Entry(canva2)
+    entrada_text.pack(pady=5)
+    label_result = tk.Label(canva2, text="Los pares son: ", fg="green", wraplength=350)
+    label_result.pack(pady=20)
+    def Analisis_connect ():
+        user_text = entrada_text.get()
+        if user_text.isdigit():
+            valor = int(user_text)
+            resultado = Analisis_num(valor)
+            label_result.config(text=f"Los pares ordenados son: {resultado}")
+        else:
+            label_result.config(text="Error, ingrese solo números, y que sean enteros.", fg="red")
+    botonw1 = tk.Button(canva2, text="Calcular pares ordenados", command=lambda: Analisis_connect())
+    botonw1.pack(pady=5)
+    botonw1_2 = tk.Button(canva2,text="Volver", command=lambda: cerrar_window1())
+    botonw1_2.pack(pady=5)
+    window1.mainloop()
+
+#Canva y otros widgets de la ventana principal
+canva1 = tk.Canvas(ventana, bg="#209ACA", width=WIDTH, height=HEIGHT)
+canva1.pack()
+
+Back_image = Image.open("resources/background.jpg").resize((WIDTH, HEIGHT))
+imgB = ImageTk.PhotoImage(Back_image)
+label_imgB = tk.Label(canva1, image=imgB, bg="#D69D17")
+label_imgB.image = imgB
+label_imgB = tk.Label(canva1, image=imgB, width=WIDTH, height=HEIGHT).pack()
+
+labelV1 = tk.Label(ventana, text="Bienvenida a mi interfaz gráfica", font=("TimesNewRoman", 15, "bold"))
+labelV1.place(x=100, y=25)
+
+botonV1 = tk.Button(canva1, text="Analisis numérico",  command=lambda: abrir_analisis_num())
+botonV1.place(x=5, y=200)
+
+botonV2 = tk.Button(canva1,text="Ficha personal", command=lambda:ficha_personal() )
+botonV2.place (x=5, y=300)
+
+ventana.mainloop()
+
 
 ventana.mainloop()
