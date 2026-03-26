9668bd41057580c31c84dd6c6579aca6046fdcf1
Francisco Li <liruizfrancisco@gmail.com>
2026-03-25 21:54:54 -0600

    Función de análisis numérico

--- main.py
+++ main.py
@@ -13,3 +13,26 @@ ANI_W = 800
 ANI_H = 500
 RADIO = 25
 
+ventana = tk.Tk()#Ventana principal
+
+ventana.title("Tkinter_Lab")
+ventana.geometry("500x500")
+ventana.resizable(False, False)
+WIDTH = 500
+HEIGHT = 500
+#Función de análisis numérico.
+def Analisis_num (num):
+    if not isinstance (num, int):
+        print("Error, ingrese un número entero.")
+        return []
+    return Analisis_num_aux(abs(num), 1)
+def Analisis_num_aux (num, divisor):
+    if divisor * divisor > num:
+        return []
+    if num % divisor == 0:
+        par = (divisor, num // divisor)
+        return [par] + Analisis_num_aux(num, divisor + 1)
+    else:
+        return Analisis_num_aux(num, divisor + 1)
+
+ventana.mainloop()
